9870fbdb7b809186dda5fe58fc4af35f076f18b5
tiyasha99 <69319639+tiyasha99@users.noreply.github.com>
2021-07-13 20:04:11 +0530

    Floor of an Element in a Sorted array

---
+++ floorOfAnElementInASortedArray.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int binSearch(int a[],int n, int num){
+  int low,high,mid;
+  low=0;
+  high=n-1;
+  int res;
+  while(low<=high){
+    mid=low+ (high-low)/2;
+    if(a[mid]==num)
+      return mid;
+    else if(num<a[mid])
+    {
+      
+      high=mid-1;
+    }
+    else
+    {
+      res=mid;
+      low=mid+1;
+    }
+      
+  }
+  return res;
+}
+int main()
+{
+  int a[]={1,2,3,4,8,10,10,11,12,19};
+  int n=10;
+  int num=5;
+  int pos=binSearch(a,n,num);
+  cout<<pos;
+}
